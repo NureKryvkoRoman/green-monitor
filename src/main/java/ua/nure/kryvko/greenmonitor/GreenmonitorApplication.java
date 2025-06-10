a62186d3f7096760bf69280036c7d6cb53732e8c
Roman Kryvko <roman.kryvko@nure.ua>
2025-06-10 21:54:58 +0200

    Initial commit.

---
+++ src/main/java/ua/nure/kryvko/greenmonitor/GreenmonitorApplication.java
@@ -0,0 +1,13 @@
+package ua.nure.kryvko.greenmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenmonitorApplication.class, args);
+	}
+
+}
